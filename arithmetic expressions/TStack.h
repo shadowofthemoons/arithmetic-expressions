892eb91378e1c1155bf41b5e4ec213155ef411d4
shadowofthemoons <zinckov.artyom@yandex.ru>
2018-11-11 20:37:01 +0300

    creating a stack class

---
+++ arithmetic expressions/TStack.h
@@ -0,0 +1,18 @@
+#pragma once
+
+
+class TStack {
+protected: // пол€
+//	PTElem pMem; // указатель на массив элементов
+	//int MemSize; // размер пам€ти дл€ —ƒ
+//	int DataCount; // количество элементов в —ƒ
+//	int Hi; // индекс вершины стека
+//	virtual int GetNextIndex(int index); // получить следующий индекс
+public:
+	//	TStack(int Size = MaxMemSize);//конструктор
+	//	~Tstack(); //деструктор
+		//int IsEmpty(void) const; // контроль пустоты
+	//	int IsFull(void) const; // контроль переполнени€
+	//	void Put(const TElem &Val);// добавить значение
+	//	virtual TElem Get(void); // 
+};
